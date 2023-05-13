472993787db9556cb0a37a2a654c3abde3b1d385
tarupathak <113367480+tarupathak@users.noreply.github.com>
2023-05-13 15:21:46 +0530

    Modified contact us form

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,4 +1,4 @@
-const newModal = ({ closePop }) => {
+const NewModal = ({ closePop }) => {
   return (
     <>
       <div className="modal-wrapper" onClick={closePop}></div>
@@ -7,31 +7,39 @@ const newModal = ({ closePop }) => {
           <span>Contact </span>
           <span className="inline">Us</span>
         </div>
-        <div>
+        <div className="inp_ut">
           <label>Name</label>
           <div>
-            <input placeholder="Enter your name" />
+            <input placeholder="Enter your name" required />
           </div>
+          <p className="valid" id="invalidName">
+            Only alphabets are allowed.
+          </p>
         </div>
-        <div>
+        <div className="inp_ut">
           <label>Email</label>
           <div>
-            <input placeholder="Enter your email" />
+            <input placeholder="Enter your email" required />
           </div>
+          <p className="valid" id="invalidEmail">
+            Enter valid email address.
+          </p>
         </div>
-        <div>
+        <div className="inp_ut">
           <label>Your Message</label>
           <div>
             <input placeholder="Enter your message" />
           </div>
         </div>
         <div id="btn-cont">
-        <span id="btn-1">Send</span>
-          <span id="btn-2" onClick={closePop}>Cancel</span>
+          <span id="btn-1">Send</span>
+          <span id="btn-2" onClick={closePop}>
+            Cancel
+          </span>
         </div>
       </div>
     </>
   );
 };
 
-export default newModal;
+export default NewModal;
