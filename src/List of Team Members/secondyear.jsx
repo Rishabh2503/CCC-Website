15356695fdf578f7a8180b5ecda38782093140ce
tarupathak <113367480+tarupathak@users.noreply.github.com>
2023-05-17 11:48:29 +0530

    Added second year members in the team page

--- src/List of Team Members/secondyear.jsx
+++ src/List of Team Members/secondyear.jsx
@@ -19,99 +19,99 @@ export const secondYear = [
         tech: "React Developer and UI/UX Designer",
         linked: "https://www.linkedin.com/in/taru-pathak-747660247/",
         github: "https://github.com/tarupathak",
-        photo: {taru}
+        photo: taru
     },
     {
         name: "Dev Vrat Tripathi",
         tech: "ML Developer",
         linked: "https://www.linkedin.com/in/dev-brat-t-ba2647108/",
         github: "https://github.com/Desi-Saitama",
-        photo: {dev}
+        photo: dev
     },
     {
         name: "Priti Kapoor",
         tech: "React Developer",
         linked: "https://www.linkedin.com/in/priti-kapoor-a10b80251/",
         github: "https://github.com/Priti2078",
-        photo: {priti}
+        photo: priti
     },
     {
         name: "Shrasti Yadav",
         tech: "UI/UX Designer",
         linked: "",
         github: "",
-        photo: {taru}
+        photo: taru
     },
     {
         name: "Sonam Kumari",
         tech: "Node.js Developer",
         linked: "https://www.linkedin.com/in/sonamkumari27",
         github: "https://github.com/sonamakgec",
-        photo: {sonam}
+        photo: sonam
     },
     {
         name: "Shashank Singh",
         tech: "Flutter Developer",
         linked: "",
         github: "",
-        photo: {shashank}
+        photo: shashank
     },
     {
         name: "Rishabh Gupta",
         tech: "React Developer",
         linked: "",
         github: "",
-        photo: {rishabh}
+        photo: rishabh
     },
     {
         name: "Saurabh",
         tech: "React Developer",
         linked: "",
         github: "",
-        photo: {saurabh}
+        photo: saurabh
     },
     {
         name: "Raghav Agarwal",
         tech: "Android Developer",
         linked: "",
         github: "",
-        photo: {raghav}
+        photo: raghav
     },
     {
         name: "Arpit Malik",
         tech: "ML Developer",
         linked: "https://www.linkedin.com/in/arpit-malik-aa836a243",
         github: "https://github.com/Arpit2221",
-        photo: {arpit}
+        photo: arpit
     },
     {
         name: "Kartik Tyagi",
         tech: "Node.js Developer",
         linked: "https://www.linkedin.com/in/kartik-tyagi-a96929205/",
         github: "https://github.com/KartikAKGEC89",
-        photo: {kartik}
+        photo: kartik
     },
     {
         name: "Sarthak Srivastava",
         tech: "React Developer",
         linked: "http://www.linkedin.com/in/sarthak-srivastava-0114a8230",
         github: "https://github.com/Sarthaksri4",
-        photo: {sarthak}
+        photo: sarthak
     },
     {
         name: "Atul Kumar",
         tech: "Node.js developer",
         linked: "",
         github: "",
-        photo: {atul}
+        photo: atul
     },
     {
         name: "Priya Sharma",
         tech: "UI/UX Designer",
         linked: "",
         github: "",
-        photo: {priya}
+        photo: priya
     }
 
-]
+];
 
--- src/Team Page/Team.jsx
+++ src/Team Page/Team.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
-import './Team.css'
-import TeamCard from '../Components/TeamCard'
-import team from '../Images/teampage.svg'
-import { secondYear } from '../List of Team Members/secondyear'
+import React from "react";
+import "./Team.css";
+import TeamCard from "../Components/TeamCard(2nd)";
+import team from "../Images/teampage.svg";
+
 
 const Team = () => {
   return (
@@ -11,30 +11,35 @@ const Team = () => {
         <img src={team} alt="team" id="illustration" />
         <div id="head">
           <div id="head_ing">
-           <span>The Cloud Computing Cell is more than just a scientific community,</span>
+            <span>
+              The Cloud Computing Cell is more than just a scientific community,
+            </span>
           </div>
-          <div className='inline' id="fam">It's a FAMILY!</div>
+          <div className="inline" id="fam">
+            It's a FAMILY!
+          </div>
         </div>
       </div>
-      <div className="title"><span>OUR </span><span className="inline">TEAM</span></div>
+      <div className="title">
+        <span>OUR </span>
+        <span className="inline">TEAM</span>
+      </div>
       <div>
         <div className="year-title">Members from Fourth Year : </div>
-        <TeamCard />
       </div>
       <div>
         <div className="year-title">Members from Third Year : </div>
-        <TeamCard />
       </div>
       <div>
         <div className="year-title">Members from Second Year : </div>
-        <TeamCard />
       </div>
+      <TeamCard />
       <div>
         <div className="year-title">Our Alumni : </div>
         <div></div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Team
+export default Team;
