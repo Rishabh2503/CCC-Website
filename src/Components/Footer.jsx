c57a3a5ee877a488d6657608e8a3b8f0e1620171
tarupathak <113367480+tarupathak@users.noreply.github.com>
2023-07-12 21:52:55 +0530

    Added footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
+import logo from "../Images/CCC_logo.svg";
+import instagram from "../Images/insta.svg";
+import linkedin from "../Images/linkedin.svg";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div id="footer">
+      <div id="foot_content">
+        <div>
+          <img src={logo} alt="ccc_logo" id="foot_logo"/>
+        </div>
+        <div id="foot_head">Cloud Computing Cell</div>
+        <div id="foot_tail">AJAY KUMAR GARG ENGINEERING COLLEGE, UTTAR PRADESH-201009</div>
+        <div id="foot-id">
+          <div>
+            <img src={instagram} alt="instgram" />
+          </div>
+          <div>
+            <img src={linkedin} alt="linkedin" />
+          </div>
+        </div>
+      </div>
+      <div id="map"></div>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
