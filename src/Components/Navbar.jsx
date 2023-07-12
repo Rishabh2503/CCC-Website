35d4800b165f2f0a80b9f93800d2680ae8061e83
tarupathak <113367480+tarupathak@users.noreply.github.com>
2023-07-13 01:54:33 +0530

    Made modifications in navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,22 +1,58 @@
 import React, { useState } from "react";
-import NewModal from "./Contact";
+import { Input } from "rsuite";
 import "./Components.css";
 import { NavLink } from "react-router-dom";
 import cccLogo from "../Images/CCC_logo.svg";
 import { Modal, ButtonToolbar, Button, Placeholder } from "rsuite";
 
 const Navbar = () => {
-  const [pop, setPop] = useState(false);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
 
-  const closePop = () => setPop(false);
-
+  const [pop, setPop] = React.useState(false);
   const [open, setOpen] = React.useState(false);
   const [size, setSize] = React.useState();
   const handleOpen = (value) => {
     setSize(value);
     setOpen(true);
   };
+  const handleShow = (value) => {
+    setSize(value);
+    setPop(true);
+  };
+  const handleExit = () => setPop(false);
   const handleClose = () => setOpen(false);
+
+  function handleName(e) {
+    if (/^[a-zA-Z ]*$/.test(e) || e === "") {
+      setName(e);
+      document.getElementById("invalidName").style.visibility = "hidden";
+      if (e === "") {
+        document.getElementById("nname").style.borderColor = "#443C68";
+      } else {
+        document.getElementById("nname").style.borderColor = "green";
+      }
+    } else {
+      document.getElementById("invalidName").style.visibility = "visible";
+      document.getElementById("nname").style.borderColor = "red";
+    }
+  }
+
+  function handleEmail(e) {
+    if (/^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$/.test(e) || e === "") {
+      setEmail(e);
+      document.getElementById("invalidEmail").style.visibility = "hidden";
+      if (e === "") {
+        document.getElementById("uname").style.borderColor = "#443C68";
+      } else {
+        document.getElementById("uname").style.borderColor = "green";
+      }
+    } else {
+      setEmail("");
+      document.getElementById("invalidEmail").style.visibility = "visible";
+      document.getElementById("uname").style.borderColor = "red";
+    }
+  }
 
   return (
     <header>
@@ -42,8 +78,77 @@ const Navbar = () => {
         </span>
         <span className="nav-item">
           <NavLink className="menu">
-            <span onClick={() => setPop(true)}>Contact Us</span>
-            {pop && <NewModal closeModal={closePop} />}
+            <span onClick={() => handleShow("sm")}>Contact Us</span>
+            <Modal size={size} open={pop} onClose={handleExit}>
+              <Modal.Header>
+                <Modal.Title>
+                  <div id="title-cont">
+                    <span>Contact </span>
+                    <span className="inline">Us</span>
+                  </div>
+                </Modal.Title>
+              </Modal.Header>
+              <Modal.Body>
+                <div>
+                  {/* <input
+                      placeholder="Enter your name"
+                      id="nname"
+                      // onClick={handleName}
+                      // onChange={handleName}
+                      required
+                    /> */}
+                  <label>Name</label>
+                  <Input
+                    placeholder="Enter your name"
+                    id="nname"
+                    onClick={handleName}
+                    onChange={handleName}
+                    required
+                  />
+                </div>
+                <p className="valid" id="invalidName">
+                  Only alphabets are allowed.
+                </p>
+
+                <div>
+                  {/* <input
+                      placeholder="Enter your email"
+                      id="uname"
+                      // onClick={handleEmail}
+                      // onChange={handleEmail}
+                      required
+                    /> */}
+                  <label>Email</label>
+                  <Input
+                    placeholder="Enter your email"
+                    id="uname"
+                    onClick={handleEmail}
+                    onChange={handleEmail}
+                    required
+                  />
+                </div>
+                <p className="valid" id="invalidEmail">
+                  Enter valid email address.
+                </p>
+
+                <label>Your Message</label>
+                <div>
+                  <Input
+                    as="textarea"
+                    rows={3}
+                    placeholder="Enter your Message"
+                  />
+                </div>
+              </Modal.Body>
+              <Modal.Footer>
+                <Button onClick={handleExit} appearance="subtle">
+                  Cancel
+                </Button>
+                <Button onClick={handleExit} appearance="primary">
+                  Send
+                </Button>
+              </Modal.Footer>
+            </Modal>
           </NavLink>
         </span>
         <span className="nav-item">
