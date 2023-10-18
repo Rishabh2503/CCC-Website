1464ef008023843b1b1d7afd9a754750878bcf41
tarupathak <113367480+tarupathak@users.noreply.github.com>
2023-10-18 12:12:55 +0530

    Fixed bugs

--- src/Home Page/Home.jsx
+++ src/Home Page/Home.jsx
@@ -12,24 +12,29 @@ import deploy from "../Images/deploy.svg";
 import Carousal2 from "./Carousal2";
 
 const Home = () => {
-
   return (
     <div>
       <div id="first-part">
-        
         <div id="head">
-          <div id="heading" style={{fontSize:"3vw"}}>
+          <div id="heading" style={{ fontSize: "3vw" }}>
             <span>Accelerating</span>
             <span className="inline"> Cloud</span>
             <span> Transformations.</span>
           </div>
-          <div id="moto" style={{fontSize:"3vw"}}>
-            <span >THINK |</span>
+          <div id="moto" style={{ fontSize: "3vw" }}>
+            <span>THINK |</span>
             <span className="inline">DEVELOP</span>
             <span>| DEPLOY</span>
           </div>
         </div>
-        <div><img src={home} alt="home" id="illustration" style={{height:"30vw"}}/></div>
+        <div>
+          <img
+            src={home}
+            alt="home"
+            id="illustration"
+            style={{ height: "30vw" }}
+          />
+        </div>
       </div>
       <div id="about-us">
         <div className="title">
@@ -38,7 +43,7 @@ const Home = () => {
         </div>
         <div id="about">
           <div id="teampic">
-            <img src={team} alt="team photo" style={{height:"30vw"}}/>
+            <img src={team} alt="team photo" style={{ height: "30vw" }} />
           </div>
           <div className="content">
             <span className="inline">Cloud Computing Cell </span>
@@ -60,8 +65,8 @@ const Home = () => {
         <span className="inline"> WE</span>
         <span> DO</span>
       </div>
-      <div id="what-we-do">
-        <div>
+      {/* <div id="what-we-do">
+        <div className="boxFlex">
           <img src={web} alt="web development" className="domain_pic" />
           <p className="domain_name">WEB DEVELOPMENT</p>
           <p className="domain_content">
@@ -70,7 +75,7 @@ const Home = () => {
             and capacity.
           </p>
         </div>
-        <div>
+        <div className="boxFlex">
           <img src={app} alt="app development" className="domain_pic" />
           <p className="domain_name">APP DEVELOPMENT</p>
           <p className="domain_content">
@@ -78,7 +83,7 @@ const Home = () => {
             react native and NetBeans and provide the required solutions
           </p>
         </div>
-        <div>
+        <div className="boxFlex">
           <img src={ml} alt="machine learning" className="domain_pic" />
           <p className="domain_name">MACHINE LEARNING</p>
           <p className="domain_content">
@@ -86,7 +91,7 @@ const Home = () => {
             capability to learn without being explicitly programmed.
           </p>
         </div>
-        <div>
+        <div className="boxFlex">
           <img src={design} alt="machine learning" className="domain_pic" />
           <p className="domain_name">UI/UX DESIGNING</p>
           <p className="domain_content">
@@ -94,34 +99,68 @@ const Home = () => {
             web and application development.
           </p>
         </div>
-      </div>
-
-      <div className="title">
-        <span>OUR</span>
-        <span className="inline"> DOMAIN</span>
-      </div>
-      <div id="what-we-do">
-        <div>
-          <img src={think} alt="think" className="domain_pic" />
-          <p className="domain_name">THINK</p>
-          <p className="domain_content">
+      </div> */}
+      <div id="whatWeDo">
+        <div className="boxFlex">
+          <img src={web} alt="web development" className="domainPic" />
+          <p className="domainName">WEB DEVELOPMENT</p>
+          <p className="domainContent">
             As web developers, we ensure that the website is visually appealing
             and easy to navigate with absolute care for website’s performance
             and capacity.
           </p>
         </div>
-        <div>
-          <img src={develop} alt="develop" className="domain_pic" />
-          <p className="domain_name">DEVELOP</p>
-          <p className="domain_content">
+        <div className="boxFlex">
+          <img src={app} alt="app development" className="domainPic" />
+          <p className="domainName">APP DEVELOPMENT</p>
+          <p className="domainContent">
             Work on different application development softwares like flutter,
             react native and NetBeans and provide the required solutions
           </p>
         </div>
-        <div>
-          <img src={deploy} alt="deploy" className="domain_pic" />
-          <p className="domain_name">DEPLOY</p>
-          <p className="domain_content">
+        <div className="boxFlex">
+          <img src={ml} alt="machine learning" className="domainPic" />
+          <p className="domainName">MACHINE LEARNING</p>
+          <p className="domainContent">
+            Machine Learning is the field of study that gives computers the
+            capability to learn without being explicitly programmed.
+          </p>
+        </div>
+        <div className="boxFlex">
+          <img src={design} alt="machine learning" className="domainPic" />
+          <p className="domainName">UI/UX DESIGNING</p>
+          <p className="domainContent">
+            UI/UX designing and graphic designing so as to provide an edge in
+            web and application development.
+          </p>
+        </div>
+      </div>
+      <div className="title">
+        <span>OUR</span>
+        <span className="inline"> DOMAIN</span>
+      </div>
+      <div id="whatWeDo">
+        <div className="boxFlex">
+          <img src={think} alt="think" className="domainPic" />
+          <p className="domainName">THINK</p>
+          <p className="domainContent">
+            As web developers, we ensure that the website is visually appealing
+            and easy to navigate with absolute care for website’s performance
+            and capacity.
+          </p>
+        </div>
+        <div className="boxFlex">
+          <img src={develop} alt="develop" className="domainPic" />
+          <p className="domainName">DEVELOP</p>
+          <p className="domainContent">
+            Work on different application development softwares like flutter,
+            react native and NetBeans and provide the required solutions
+          </p>
+        </div>
+        <div className="boxFlex">
+          <img src={deploy} alt="deploy" className="domainPic" />
+          <p className="domainName">DEPLOY</p>
+          <p className="domainContent">
             Machine Learning is the field of study that gives computers the
             capability to learn without being explicitly programmed.
           </p>
